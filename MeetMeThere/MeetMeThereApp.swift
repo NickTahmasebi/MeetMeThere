bd607522d303f8b9d19cd6b04c756d1b47529752
NickTahmasebi <nicktahmasebi@hotmail.com>
2023-02-16 16:06:34 +0100

    Initial Commit

---
+++ MeetMeThere/MeetMeThereApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetMeThereApp.swift
+//  MeetMeThere
+//
+//  Created by Nick Tahmasebi on 16/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct MeetMeThereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
