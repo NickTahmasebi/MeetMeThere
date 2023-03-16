53ff0f377c47a390c84e5dc786dd962d57ee4176
MarcoHansen7 <s194302@student.dtu.dk>
2023-03-16 16:23:41 +0100

    Test af git

---
+++ MeetMeThere/SletDette.swift
@@ -0,0 +1,20 @@
+//
+//  SletDette.swift
+//  MeetMeThere
+//
+//  Created by Marco on 16/03/2023.
+//
+
+import SwiftUI
+
+struct SletDette: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SletDette_Previews: PreviewProvider {
+    static var previews: some View {
+        SletDette()
+    }
+}
