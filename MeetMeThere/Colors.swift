9e280c10f674e852e8b22c7e9300720662db90a9
Sofiedige <sofiedige1@gmail.com>
2023-03-16 15:41:55 +0100

    colors added

---
+++ MeetMeThere/Colors.swift
@@ -0,0 +1,23 @@
+//
+//  Colors.swift
+//  SwiftApp
+//
+//  Created by Sofie Groth Dige on 09/03/2023.
+//
+
+import SwiftUI
+
+extension Color{
+
+    public static let bgColor: Color = Color(red: 251 / 255, green: 234 / 255, blue: 254 / 255)
+    public static let oceanBlueColor: Color = Color(red: 49/255, green: 117/255, blue: 207/255)
+    public static let skyBlueColor: Color = Color(UIColor(red: 20/255, green: 131/255, blue: 204/255, alpha: 1.0))
+    public static let BlueColor: Color = Color(UIColor(red: 20/255, green: 131/255, blue: 204/255, alpha: 1.0))
+    public static let seaBlueColor: Color = Color(UIColor(red: 20/255, green: 138/255, blue: 184/255, alpha: 1.0))
+    public static let greenOceanColor: Color = Color(UIColor(red: 20/255, green: 142/255, blue: 154/255, alpha: 1.0))
+    public static let greenBlueColor: Color = Color(UIColor(red: 20/255, green: 142/255, blue: 122/255, alpha: 1.0))
+    public static let PurpleColor: Color = Color(UIColor(red: 216/255, green: 216/255, blue: 216/255, alpha: 1.0))
+    public static let lightPurpleColor: Color = Color(UIColor(red: 179/255, green: 157/255, blue: 206/255, alpha: 1.0))
+    public static let koralColor: Color = Color(UIColor(red: 32/255, green: 200/255, blue: 167/255, alpha: 1.0))
+
+}
